792560439fcd53a52c8b4ab7b0d9537ab911fb51
muhammadsaadali <saadalikhan96@gmail.com>
2021-01-05 12:34:53 +0500

    Working

--- src/shared/components/login/LoginCard.jsx
+++ src/shared/components/login/LoginCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'reactstrap';
 import FirebaseIcon from 'mdi-react/FirebaseIcon';
@@ -9,14 +9,50 @@ import LogInForm from './LogInForm';
 import GoogleAuthBtn from '../../../containers/Account/AuthBtn/googleAuthBtn';
 import FacebookAuthBtn from '../../../containers/Account/AuthBtn/fbAuthBtn';
 
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import firebase from '../../../config/firebase'
+// Add the Firebase services that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+// Firebase Config
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+
+
 const auth0Icon = `${process.env.PUBLIC_URL}/img/auth0.svg`;
 const LoginCard = ({ changeIsOpenModalFireBase }) => {
+  
+
+
   const {
     loginWithRedirect, loading,
   } = useAuth0();
   if (loading) {
     return (<Loading loading={loading} />);
   }
+  // Make a method to check the authenticity, and also make state to check whether User is here or not
+      const [user,setUser] = useState(null);
+      const [email,setEmail] =useState(null);
+      const [password,setPassword] =useState(null);
+
+      function authListener(){
+        firebase.auth().onAuthStateChanged((us)=> {
+          if(us){
+            setUser(us)
+          }
+          else{ 
+            setUser(null) 
+          }
+        })
+      }
+        // Use effect for authentication 
+        useEffect(() => authListener(),[])
+      // Make login function
+      
+
   return (
     <div className="account__wrapper">
       <div className="account__card">
@@ -28,6 +64,7 @@ const LoginCard = ({ changeIsOpenModalFireBase }) => {
           </h3>
           <h4 className="account__subhead subhead">Start your business easily</h4>
         </div>
+        {user ? <h1> User Logged in </h1> : <h1> User not Logged in </h1> }
         <LogInForm
           onSubmin
           form="log_in_form"
